382954cf898878b2053b168368457a31dc8a4f1f
Evgenii Dudnik <johnydvgu@gmail.com>
2023-01-12 15:33:19 +0300

    add dev server

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,13 +1,38 @@
 import webpack from "webpack";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import {BuildOptions} from "./types/config";
 
-export default function(): webpack.RuleSetRule[] {
+export default function (options: BuildOptions): webpack.RuleSetRule[] {
+    const {isDev} = options;
+
     const tsLoader = {
         test: /\.tsx?$/,
         use: 'ts-loader',
         exclude: /node_modules/,
     };
 
+    const cssLoaders = {
+        test: /\.s[ac]ss$/i,
+        use: [
+            // Creates `style` nodes from JS strings
+            isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+            // Translates CSS into CommonJS
+            {
+                loader: "css-loader",
+                options: {
+                    modules: {
+                        auto: (resPah: string) => Boolean(resPah.includes('.module.')),
+                        localIdentName: isDev ? '[path][name]__[local]' : '[hash:base64:8]'
+                    },
+                }
+            },
+            // Compiles Sass to CSS
+            "sass-loader",
+        ],
+    };
+
     return [
         tsLoader,
+        cssLoaders,
     ];
 }
--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,5 +1,6 @@
 import HTMLWebpackPlugin from "html-webpack-plugin";
 import webpack from "webpack";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
 
 export function buildPlugins(template: string): webpack.WebpackPluginInstance[] {
     return [
@@ -7,5 +8,9 @@ export function buildPlugins(template: string): webpack.WebpackPluginInstance[] 
             template,
         }),
         new webpack.ProgressPlugin(),
+        new MiniCssExtractPlugin({
+            filename: 'css/[name].[contenthash:8].css',
+            chunkFilename: 'css/[name].[contenthash:8].css',
+        }),
     ];
 }
